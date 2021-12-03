58ef9987c526dabeba4ceadf2e88706a850cc17e
Akihisa_Watanabe <akihisa@ruri.waseda.jp>
2021-12-03 21:23:59 +0900

    Add my solutions

---
+++ functions/functions5.rs
@@ -0,0 +1,17 @@
+// functions5.rs
+// Make me compile! Execute `rustlings hint functions5` for hints :)
+
+
+fn main() {
+    let answer = square(3);
+    println!("The answer is {}", answer);
+}
+
+fn square(num: i32) -> i32 {
+    num * num 
+    /* 
+    ; をつけるとCでいうvoidみたいに振る舞う．
+    別解：return num * num
+    */
+    
+}
