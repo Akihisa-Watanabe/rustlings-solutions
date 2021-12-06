4e14e51d28d9148ad64653ff08cf59317d38e99b
Akihisa_Watanabe <akihisa@ruri.waseda.jp>
2021-12-06 16:17:51 +0900

    Add my solutions

--- structs/structs3.rs
+++ structs/structs3.rs
@@ -16,6 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
+            panic!("weight_in_grams must be a positive number.");
             // Something goes here...
         } else {
             return Package {
@@ -26,12 +27,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if &self.sender_country != &self.recipient_country{
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        &self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -73,7 +78,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = ???;
+        let cents_per_gram = 3;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
